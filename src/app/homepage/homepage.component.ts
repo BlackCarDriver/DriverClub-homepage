7e7d9b22769c3696ae0ee9a1058c1a1baa4caf81
BlackCarDriver <13169346002@163.com>
2019-06-01 17:19:31 +0800

    add bootstarp and jquery

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $:any;
+
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
+    
   }
+
+  show(){
+    let footer = document.getElementById('foot');
+    footer.style.height="100px";
+  }
+  hide(){
+    let footer = document.getElementById('foot');
+    footer.style.height="30px";
+  }
+
 
 }
